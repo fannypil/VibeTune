586a5166cae370f1059fd14b329bcf9409324a02
Fanny Pilnik <fanysh3@gmail.com>
2025-06-07 13:30:58 +0300

    Add user routes

--- backend/main.py
+++ backend/main.py
@@ -4,14 +4,15 @@ from routes.playlist import router as playlist_router
 from routes.lastfm import router as lastfm_router
 from routes.auth import router as auth_router
 from routes.playlist import router as playlist_router
+from routes.user import router as user_router
+from db.session import engine
+from db.models import Base
+
+
+# Create database tables
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-# Importing the playlist router
-app.include_router(playlist_router)
-app.include_router(lastfm_router)
-app.include_router(auth_router)
-app.include_router(playlist_router)
 
 # Allow CORS for frontend
 app.add_middleware(CORSMiddleware,
@@ -19,6 +20,12 @@ app.add_middleware(CORSMiddleware,
                     allow_credentials=True,
                     allow_methods=["*"],
                     allow_headers=["*"])
+
+# Importing the playlist router
+app.include_router(playlist_router)
+app.include_router(lastfm_router)
+app.include_router(auth_router)
+app.include_router(user_router)
 
 @app.get("/", response_model=dict)
 async def root():
